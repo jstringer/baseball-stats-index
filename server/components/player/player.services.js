0765daf0a65600588bc1f07c2b86027097616276
Jack Stringer <jackstringer@gmail.com>
2021-01-10 19:23:50 -0800

    Updated autocomplete route

--- server/components/player/player.services.js
+++ server/components/player/player.services.js
@@ -8,17 +8,8 @@ const getPlayerByFirstAndLastName = function(nameFirst, nameLast) {
 }
 
 const getPlayerByPartial = function(term) {
-  return db('Baseball.People').select('nameFirst', 'nameLast')
-  .where( (builder) => {
-    if(term[1]) {
-      return builder
-        .where('nameFirst', 'like', `%${term[0]}%`)
-        .where('nameLast', 'like', `%${term[1]}%`);
-    }
-    return builder
-      .where('nameFirst', 'like', `%${term[0]}%`)
-      .orWhere('nameLast', 'like', `%${term[0]}%`);
-  });
+  return db('Baseball.People').select(db.raw("CONCAT_WS(' ', \"nameFirst\", \"nameLast\") AS nameFull"))
+    .where(db.raw("CONCAT_WS(' ', \"nameFirst\", \"nameLast\")"), 'like', `%${term}%`)
 }
 
 module.exports = {
