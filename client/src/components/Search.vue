d7742e1ef37909a1739b724e9256797c6db94ea6
Jack Stringer <jackstringer@gmail.com>
2021-01-18 11:19:57 -0800

    Added routing skeleton, started autocomplete

---
+++ client/src/components/Search.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="search">
+    <input 
+      type="text"
+      v-model="searchTerm"
+      @input="onInput"
+      :placeholder="placeholder"
+    />
+    <ul class="autocomplete-results">
+      <li class="autocomplete-result">
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+   name: 'search',
+   props: ['placeholder'],
+   data() {
+     return {
+       searchTerm: '',
+       results: [],
+       isTyping: false
+     };
+   },
+   methods: {
+     onInput() {
+
+     }
+   }
+}
+</script>
+
+<style>
+
+  .search {
+    position: relative;
+    width: 130px;
+  }
+
+  .autocomplete-results {
+    border: 1px solid #000000;
+    overflow: auto;
+  }
+
+  .autocomplete-result {
+    text-align: left;
+    padding: 4px 2px;
+    cursor: pointer;
+  }
+
+  .autocomplete-result:hover {
+    background-color: #4AAE9B;
+  }
+
+</style>
